360cdd719ed915e79e5fa31f826280d3e376cde5
danielmihaylov <daniel.a.mihaylov@abv.bg>
2017-02-09 22:10:22 +0200

    Questions backButton

--- src/application/controllers/QuestionsController.java
+++ src/application/controllers/QuestionsController.java
@@ -8,21 +8,26 @@ import application.classes.GameManager;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.IOException;
+import java.util.*;
 
 
 public class QuestionsController {
+    @FXML
+    Button backButton2;
     @FXML
     Label questionNumLabel;
     @FXML
@@ -216,5 +221,12 @@ public class QuestionsController {
 
         hintOne.setDisable(true);
 
+    }
+
+    public void OnBack(ActionEvent actionEvent) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("../resources/fxml/cities.fxml"));
+            Stage stage = (Stage)backButton2.getScene().getWindow();
+            stage.setScene(new Scene(root, Values.SCREEN_WIDTH,Values.SCREEN_HEIGHT));
+            stage.show();
     }
 }
